3f07f67d25ac2b2d9736ec4b9d859acbf6b0e457
kujtimsaliu <kujtimsaliu011@gmail.com>
2024-03-24 02:42:52 +0100

    first commit

---
+++ Extensions/UIViewController+Ext.swift
@@ -0,0 +1,36 @@
+//
+//  UIViewController+Ext.swift
+//  GHFollowers
+//
+//  Created by Sean Allen on 12/30/19.
+//  Copyright © 2019 Sean Allen. All rights reserved.
+//
+
+import UIKit
+
+fileprivate var containerView: UIView!
+
+extension UIViewController {
+    
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = GFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle  = .overFullScreen
+            alertVC.modalTransitionStyle    = .crossDissolve
+            self.present(alertVC, animated: true)
+        }
+    }
+    
+    
+    func showLoadingView() { }
+    
+    
+    func dismissLoadingView() { }
+    
+    
+    func showEmptyStateView(with message: String, in view: UIView) {
+        let emptyStateView = GFEmptyStateView(message: message)
+        emptyStateView.frame = view.bounds
+        view.addSubview(emptyStateView)
+    }
+}
